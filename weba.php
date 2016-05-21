ddfff50f718e08c0eafcc7113e50a9c091e6089c
RodrigoAlonso <alonzo_5sepro@hotmail.com>
2016-05-20 21:00:28 -0500

    Update weba.php

--- weba.php
+++ weba.php
@@ -16,12 +16,10 @@ echo "
          \_/\_/  |_____|____/_/   \_\
         
        [*] Author : Rodrigo Canaz. \"RACP\"
-       [*] Update : 30/4/2016
-       [*] Version : 0.0.2
+       [*] Date : 28/4/2016
+       [*] Version : 0.0.1 
 
 ";
-system('rm LIST.txt');
-system('touch LIST.txt');
 if(isset($argv[1])){
 echo "TARGET : " . $argv[1] . "\n";
 $url = "http://" . "$argv[1]" . "/";
@@ -31,20 +29,20 @@ $ip_comp = gethostbyname("$argv[1]");
 $ip = explode(".",$ip_comp);
 echo "[+]Starting brute force ! \n";
 $num = 0;
+$file=fopen("LIST.txt","w+");
 while ($num <= 255 ) {
  $res = $num++;
-$file = fopen("LIST.txt","a");
 fwrite($file,"$ip[0]." . "$ip[1]." . "$ip[2]." . "$res:");
-} 
+}
 fclose($file);
-$rata = fopen("LIST.txt","r");
+$rata = fopen("LIST.txt","r"); 
 while(!feof($rata)){
 $lata = fgets($rata); 
 $lata_1 = explode(":",$lata); 
 for ( $i = 0 ; $i <=255 ; $i++ ){
 echo "\n\n=====================================\n";
 echo "[-] Probando IP : $lata_1[$i]\n\n ";
-$tati = "http://" . "$lata_1[$i]" . "/";
+$tati = "http://" . "$lata_1[$i]" . "/"; # NOMBRE DE UNA AMIGA :# 
 $url = "$tati";
 $putas = curl_init($url);
 curl_setopt($putas, CURLOPT_URL , $url);
@@ -52,7 +50,7 @@ curl_setopt($putas, CURLOPT_RETURNTRANSFER,1);
 curl_setopt($putas, CURLOPT_TIMEOUT,10);
 curl_setopt($putas ,CURLOPT_RETURNTRANSFER,true);
 $output = curl_exec($putas);
-$httpcode = curl_getinfo($putas, CURLINFO_HTTP_CODE); //PIDIENDO CODIGO !
+$httpcode = curl_getinfo($putas, CURLINFO_HTTP_CODE); 
 curl_close($putas);
 echo '[!]' . date('h:i:s');
 if (strlen($output) > 0){
@@ -257,9 +255,14 @@ break;
 default:
 echo " - NULL";
 break;
-}
+
+} 
 }
 fclose($rata);
+
+
+
 }
+ 
 } 
 ?>
